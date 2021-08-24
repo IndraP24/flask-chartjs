6855f7b2861e011099139c335bca96f488191ead
IndraP24 <indrashis985@gmail.com>
2021-08-25 00:04:00 +0530

    Add main method to run flask app

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ from flask import Flask, render_template
 from datetime import time
 
 app = Flask(__name__)
+
+
+@app.route("/simple_chart")
+def chart():
+    legend = "Monthly Data"
+    labels = ["January", "February", "March",
+              "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
